2d850a2dc5d09b6663512cea084e590ff7994ff8
mingoxd <136061511+mingoxd@users.noreply.github.com>
2023-07-11 10:54:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ with col1:
     physics_med_chem = data_sorted[data_sorted['Category'].isin(['Chemistry', 'Physics', 'Medicine'])]
     fig1 = px.box(physics_med_chem, y="Age", x="Category", color="Category", color_discrete_map=category_colors)
     fig1.update_layout(showlegend=False)  # Remove legend from the first plot
-    tab2.plotly_chart(fig1, use_container_width=True)
+    st.plotly_chart(fig1, use_container_width=True)
 
     # Add label below the first boxplot
     st.subheader("Natural Sciences")
@@ -135,7 +135,7 @@ with col2:
     lit_peace_econ = data_sorted[data_sorted['Category'].isin(['Literature', 'Peace', 'Economics'])]
     fig2 = px.box(lit_peace_econ, y="Age", x="Category", color="Category", color_discrete_map=category_colors)
     fig2.update_layout(showlegend=False)  # Remove legend from the second plot
-    tab2.plotly_chart(fig2, use_container_width=True)
+    st.plotly_chart(fig2, use_container_width=True)
 
     # Add label below the second boxplot
     st.subheader("Social Sciences")
